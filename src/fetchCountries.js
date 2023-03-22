aaae4c6c160934c57c3469ac942b7baac59052ca
PashkaTurzhanskyi <pavlo.turzhanskiy@gmail.com>
2023-03-22 21:02:21 +0200

    add HW10

---
+++ src/fetchCountries.js
@@ -0,0 +1,9 @@
+export const fetchCountries = name =>
+  fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
 import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchCountries } from './fetchCountries';
+var debounce = require('lodash.debounce');
 
 const DEBOUNCE_DELAY = 300;
+const countryListEl = document.querySelector('ul.country-list');
+countryListEl.previousElementSibling.addEventListener(
+  'input',
+  debounce(onInput, DEBOUNCE_DELAY)
+);
+
+function onInput() {
+  const nameCountry = countryListEl.previousElementSibling.value.trim();
+  if (nameCountry === '' || nameCountry === ' ') {
+    countryListEl.nextElementSibling.innerHTML = '';
+    countryListEl.innerHTML = '';
+    return;
+  } else {
+    fetchCountries(nameCountry)
+      .then(country => {
+        if (country.length > 10) {
+          Notify.info(
+            'Too many matches found. Please enter a more specific name.'
+          );
+          countryListEl.innerHTML = '';
+          countryListEl.nextElementSibling.innerHTML = '';
+        } else if (country.length > 2 && country.length <= 10) {
+          countryListEl.nextElementSibling.innerHTML = '';
+          countryListEl.innerHTML = country
+            .map(i => {
+              return `<div class="country-list_flag"><img src="${i.flags.svg}" alt="" width="32" /><div class="country-list_title">${i.name.common}</div></div>`;
+            })
+            .join('');
+        } else {
+          countryListEl.innerHTML = '';
+          countryListEl.nextElementSibling.innerHTML = `<div class="country-info_flag"><img src="${
+            country[0].flags.svg
+          }" alt="" width="32" /><div class="country-info_title">${
+            country[0].name.common
+          }</div></div><div class="country-info_description"><b>Capital:</b> ${
+            country[0].capital[0]
+          }</div><div class="country-info_description"><b>Population:</b> ${
+            country[0].population
+          }</div><div class="country-info_description"><b>Languages:</b> ${
+            Object.values(country[0].languages)[0]
+          }</div>`;
+        }
+      })
+      .catch(error => {
+        Notify.failure('Oops, there is no country with that name');
+        countryListEl.innerHTML = '';
+        countryListEl.nextElementSibling.innerHTML = '';
+      });
+  }
+}
